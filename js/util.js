98184e82c32e3d294dee841a34d6428506617265
DVNst <dvn-studio@mail.ru>
2018-12-21 22:11:34 +0300

    удаляем неиспользуемые функции

--- js/util.js
+++ js/util.js
@@ -2,7 +2,6 @@
 
 (function () {
   var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
 
   window.util = {
     isEscEvent: function (evt, action) {
@@ -10,43 +9,8 @@
         action();
       }
     },
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action();
-      }
-    },
     getRandomNumber: function (min, max) {
       return Math.round(Math.random() * (max - min) + min);
-    },
-    getShuffledArray: function (arr, length) {
-      var arrMix = [];
-
-      if (!length) {
-        length = arr.length;
-      }
-
-      for (var i = 0; i < length; i++) {
-        var index = this.getRandomNumber(0, arr.length - 1);
-        arrMix.push(arr[index]);
-        arr.splice(index, 1);
-      }
-
-      return arrMix;
-    },
-    generateArrayNumber: function (min, max) {
-      if (min > max) {
-        max = [min, min = max][0];
-      }
-
-      var arr = [];
-
-      for (var i = min; i <= max; i++) {
-        arr.push(i);
-      }
-      return arr;
-    },
-    getRandomArrayElement: function (arr) {
-      return arr[this.getRandomNumber(0, arr.length - 1)];
     }
   };
 })();
